4e23acf3a4c50bec9640f47d982b616e49d5ba20
jmacalawa <jmacalawa@futurex.ph>
2022-01-31 15:53:06 +0800

    release

--- components/profile/ProfileContainer.vue
+++ components/profile/ProfileContainer.vue
@@ -1,5 +1,83 @@
 <template>
   <div align="center">
+    <v-dialog v-model="deleteConfirmation" width="500" persistent>
+    <v-card class="pa-10">
+    <div align="center" class="text-h6">Confirmation</div>
+    <div align="center" class="pa-10">
+        Are you sure you want to delete this item?
+    </div>
+      <v-card-actions>
+        <v-row align="center">
+            <v-col align="end">
+                <v-btn color="red" text @click="deleteConfirmation=false"> Cancel </v-btn>
+            </v-col>
+            <v-col>
+                <v-btn color="success" text :loading="buttonLoad" @click="deleteAnnouncement"> Confirm </v-btn>
+            </v-col>
+        </v-row>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+    <v-dialog v-model="editDiscussion" width="1000" persistent>
+  <v-form v-model="valid"
+      ref="form"
+      lazy-validation
+      @submit.prevent="addDiscussions">
+    <v-card class="pa-10">
+      <div align="center" class="text-h6">Edit Post</div>
+      <div class="text-h6">Title</div>
+      <div>
+        <v-text-field :rules="standardRules"  outlined v-model="discussions.title"></v-text-field>
+      </div>
+      <div class="text-h6">Descriptions</div>
+      <div>
+        <v-textarea outlined v-model="discussions.descriptions"></v-textarea>
+      </div>
+      <v-col>
+        <span class="pt-2 pr-10 pb-10"><b>Upload Image</b></span>
+
+        <div class="hover_pointer pt-10">
+          <img
+            @click="$refs.file.click()"
+            :src="img_holder"
+            alt="item_.js"
+            height="150"
+            class="mb-0"
+          />
+        </div>
+      </v-col>
+      <v-col class="d-none">
+        <input
+          style="display: none"
+          type="file"
+          id="fileInput"
+          ref="file"
+          accept="image/png, image/jpeg"
+          @change="onFileUpload"
+        />
+      </v-col>
+
+      <v-card-actions>
+        <v-row align="center">
+          <v-col align="end">
+            <v-btn color="red" text @click="cancel"> Cancel </v-btn>
+          </v-col>
+          <v-col>
+            <v-btn
+              color="success"
+              text
+              type="submit"
+          :disabled="!valid"
+              :loading="buttonLoad"
+            >
+              Save
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-actions>
+    </v-card>
+  </v-form>
+  </v-dialog>
     <v-dialog v-model="editMarket" width="1000" persistent>
     <v-form
       v-model="valid"
@@ -8,7 +86,7 @@
       @submit.prevent="addMarketplace"
     >
       <v-card class="pa-10">
-        <div align="center" class="text-h6">Add Marketlist</div>
+        <div align="center" class="text-h6">Edit Marketlist</div>
         <div class="text-h6">Name of Listing</div>
         <div>
           <v-text-field
@@ -132,7 +210,7 @@
         </div>
         <v-row>
           <v-col
-            v-for="index in discussions"
+            v-for="index in items_discussions"
             :key="index"
             cols="4"
             class="pa-5"
@@ -146,38 +224,26 @@
                   indeterminate
                 ></v-progress-linear>
               </template>
-              <v-img height="50" :src="index.image"></v-img>
+              <v-img height="150" :src="index.image"></v-img>
               <v-card-text>
                 <div>
                   {{ index.descriptions }}
                 </div>
               </v-card-text>
               <v-divider class="mx-4"></v-divider>
-              <v-card-title>Tags</v-card-title>
-
-              <v-card-text>
-                <v-chip-group
-                  active-class="deep-purple accent-4 white--text"
-                  column
-                >
-                  <v-chip>5:30PM</v-chip>
-
-                  <v-chip>7:30PM</v-chip>
-
-                  <v-chip>8:00PM</v-chip>
-
-                  <v-chip>9:00PM</v-chip>
-                </v-chip-group>
-              </v-card-text>
+              
 
               <v-card-actions>
                 <v-btn
                   color="deep-purple lighten-2"
                   text
-                  @click="viewItem(index)"
+                  @click="viewItemDiscussion(index)"
                 >
                   View
                 </v-btn>
+                <v-icon color="red" @click="deleteItem(index,'discussion')">
+                  mdi-delete
+                </v-icon>
               </v-card-actions>
             </v-card>
           </v-col>
@@ -218,6 +284,7 @@
               >
                 View Event
               </v-btn>
+              
             </div>
           </v-card>
         </v-col>
@@ -232,7 +299,7 @@
         type="card"
       ></v-skeleton-loader>
         <v-row v-else>
-          <v-col v-for="key in marketplace" :key="key" cols="4" class="pa-5">
+          <v-col v-for="key in items_marketplace" :key="key" cols="4" class="pa-5">
           <v-card class="mx-auto my-12" max-width="374">
             <template slot="progress">
               <v-progress-linear
@@ -252,6 +319,9 @@
             <v-card-title>Php {{parseFloat((key.price)).toFixed(2)}}</v-card-title>
             <v-card-actions>
               <v-btn color="deep-purple lighten-2" text @click="viewItemMarket(key)"> View </v-btn>
+               <v-icon color="red" @click="deleteItem(key,'marketplace')">
+                  mdi-delete
+                </v-icon>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -261,33 +331,191 @@
   </div>
 </template>
 <script>
+import validations from "@/utils/validations";
 export default {
   data() {
     return {
+      deleteConfirmation:false,
+      items_discussions:[],
+      ...validations,
       name: "",
+      buttonLoad:false,
       valid:false,
       marketplace:[],
       email: "",
       address: "",
       discussions: [],
-      img_holder1:'',
+      img_holder_qr:'',
       img_holder:'',
       isLoading: false,
       events:[],
+      items_marketplace:[],
+      image_qr:'',
       marketplace:[],
+      selectedItem:[],
+      category:'',
       profileImage:'',
       editMarket:false,
+      editDiscussion:false
     };
   },
   created() {
     this.loadData();
   },
   methods: {
+      async deleteAnnouncement(){
+     this.buttonLoad=true
+     if(this.category=='discussion'){
+        this.$axios.delete(`/discussions/${this.selectedItem.id}/`,{
+        headers:{
+          Authorization:`Bearer ${localStorage.getItem('token')}`
+        }
+      })
+      .then(()=>{
+          this.deleteConfirmation=false
+          this.buttonLoad=false
+          alert('Successfully Deleted!')
+          this.loadData()
+      })
+     }
+     else{
+       this.$axios.delete(`/marketplace/${this.selectedItem.id}/`,{
+        headers:{
+          Authorization:`Bearer ${localStorage.getItem('token')}`
+        }
+      })
+      .then(()=>{
+          this.deleteConfirmation=false
+          this.buttonLoad=false
+          alert('Successfully Deleted!')
+          this.loadData()
+      })
+     }
+      
+    },
+     deleteItem(val,category){
+      this.selectedItem=val
+      this.category=category
+      this.deleteConfirmation=true
+    },
+    async addDiscussions() {
+      // this.valid = this.$refs.form.validate();
+      // if (!this.valid) {
+      //   return;
+      // }
+      this.buttonLoad = true;
+      try {
+        let form_data = new FormData();
+        if (this.image != null && this.image != "") {
+          form_data.append("image", this.image);
+        }
+        form_data.append("user_id", localStorage.getItem("id"));
+        form_data.append("title", this.discussions.title);
+        form_data.append("descriptions", this.discussions.descriptions)
+          const response = await this.$axios
+            .patch(`/discussions/${this.discussions.id}/`, form_data, {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+              },
+            })
+            .then(() => {
+              this.buttonLoad = false;
+              this.editDiscussion=false
+            });
+        
+      } catch (error) {
+        alert(error);
+        this.buttonLoad = false;
+      }
+    },
+    async addMarketplace() {
+      this.valid = this.$refs.form.validate();
+      if (!this.valid) {
+        return;
+      }
+      this.buttonLoad = true;
+      try {
+        let form_data = new FormData();
+        if (this.image != null && this.image != "") {
+          form_data.append("image", this.image);
+        }
+        if (this.image_qr != null && this.image_qr != "") {
+          form_data.append("image_qr", this.image_qr);
+        }
+        form_data.append("name", this.marketplace.name);
+        form_data.append("category", this.marketplace.category);
+        form_data.append("price", this.marketplace.price);
+        form_data.append("descriptions", this.marketplace.descriptions);
+          const response = await this.$axios
+            .patch(`/marketplace/${this.marketplace.id}/`, form_data, {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+              },
+            })
+            .then(() => {
+              this.buttonLoad = false;
+              this.editMarket=false
+            });
+        
+      } catch (error) {
+        alert(error);
+        this.buttonLoad = false;
+      }
+    },
+    onFileUploadQR(e) {
+      this.image_qr = e;
+      e = e.target.files[0];
+      if (e["name"].length > 100) {
+        alert("255 characters exceeded filename.");
+        return;
+      }
+      try {
+        if (e.size > 16000000) {
+          alert("Only 15mb file can be accepted.");
+          return;
+        }
+      } catch (error) {
+        alert(error);
+        return;
+      }
+      this.image_qr = e;
+      if (e == null) {
+      } else {
+        this.url, (this.img_holder_qr = URL.createObjectURL(e));
+      }
+    },
+        onFileUpload(e) {
+      this.image = e;
+      e = e.target.files[0];
+      if (e["name"].length > 100) {
+        alert("255 characters exceeded filename.");
+        return;
+      }
+      try {
+        if (e.size > 16000000) {
+          alert("Only 15mb file can be accepted.");
+          return;
+        }
+      } catch (error) {
+        alert(error);
+        return;
+      }
+      this.image = e;
+      if (e == null) {
+      } else {
+        this.url, (this.img_holder = URL.createObjectURL(e));
+      }
+    },
     viewItemMarket(item){
       this.img_holder = item.image
-      this.img_holder1=item.image1
+      this.img_holder_qr=item.image_qr
       this.marketplace=item
       this.editMarket=true
+    },
+    viewItemDiscussion(item){
+      this.img_holder = item.image
+      this.discussions=item
+      this.editDiscussion=true
     },
     loadData() {
       this.name = localStorage.getItem("firstname")+' '+localStorage.getItem("lastname");
@@ -307,7 +535,7 @@ export default {
           },
         })
         .then((res) => {
-          this.marketplace = res.data;
+          this.items_marketplace = res.data;
           this.isLoading = false;
         });
     },
@@ -320,7 +548,7 @@ export default {
           },
         })
         .then((res) => {
-          this.discussions = res.data;
+          this.items_discussions = res.data;
           this.isLoading = false;
         });
     },
--- components/reports/ReportsForm.vue
+++ components/reports/ReportsForm.vue
@@ -1,53 +1,21 @@
 <template>
   <div align="center" class="pa-10">
     <v-card elevation="2" width="900" class="pa-5">
-    <div style="height:350px">
+    <!-- <div style="height:350px">
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfTLNWTsRAlZhpqzf5_3Ua8zUuDgIeOLZBEZYqC-x_YQqpe2Q/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Google Forms is Loading…</iframe>
-    </div>
-      <!-- <v-row>
-        <v-col class="pa-5">
-          <span
-            class="text-h4"
-            style="cursor: pointer"
-            @click="dialogAdd = true"
-          >
-            Reports Form
-          </span>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-            <div align="start">
-                Name(Optional):
-            </div>
-            <div>
-                <v-text-field outlined placeholder="Ex:Juan Delacruz"></v-text-field>
-            </div>
-        </v-col>
-        <v-col cols="12">
-            <div align="start">
-                Descriptions
-            </div>
-            <div>
-                <v-textarea outlined>
-
-                </v-textarea>
-            </div>
-        </v-col>
-        
-      </v-row>
-      <div class="pt-5" >
+    </div> -->
+    <div class="pt-5">
               <v-btn
+                @click="feedback"
                 x-large
-                color="black"
                 width="200"
-                dark
                 outlined
                 height="40"
+                :loading="isLoaded"
               >
-                Submit
+                Feedback
               </v-btn>
-            </div> -->
+            </div>
     </v-card>
   </div>
 </template>
@@ -59,6 +27,11 @@ export default {
       dialogAdd: false,
     };
   },
+  methods:{
+    feedback(){
+      window.location.href="https://docs.google.com/forms/d/e/1FAIpQLSfTLNWTsRAlZhpqzf5_3Ua8zUuDgIeOLZBEZYqC-x_YQqpe2Q/viewform?embedded=true"
+    }
+  }
 };
 </script>
 
